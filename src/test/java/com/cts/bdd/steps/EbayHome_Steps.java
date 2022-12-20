b0a5fe33dc82fc11101be1a520cfed385843d486
satyaki726 <satyakisaha726@gmail.com>
2022-12-20 18:49:40 +0530

    Updated the steps with selenium logics

--- src/test/java/com/cts/bdd/steps/EbayHome_Steps.java
+++ src/test/java/com/cts/bdd/steps/EbayHome_Steps.java
@@ -1,25 +1,41 @@
 package com.cts.bdd.steps;
 
+import static org.junit.Assert.fail;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class EbayHome_Steps {
+	
+	WebDriver driver;
+	
+	public EbayHome_Steps(Common_Steps common_Steps) {
+		this.driver = common_Steps.getDriver();
+	}
+	
 	@Given("I am on Ebay Home Page")
 	public void i_am_on_ebay_home_page() {
 	    // Write code here that turns the phrase above into concrete actions
-	    System.out.println("I am on Ebay Home Page");
+		driver.get("https://www.ebay.com/");
 	}
 
 	@When("I click on Advanced Link")
 	public void i_click_on_advanced_link() {
 	    // Write code here that turns the phrase above into concrete actions
-		System.out.println("I click on Advanced Link");
+		driver.findElement(By.linkText("Advanced")).click();
 	}
 
 	@Then("I navigate to Advaned Search Page")
 	public void i_navigate_to_advaned_search_page() {
 	    // Write code here that turns the phrase above into concrete actions
-	    System.out.println("I navigate to Advaned Search Page");
+	    String expUrl = "https://www.ebay.com/sch/ebayadvsearch";
+	    String actUrl = driver.getCurrentUrl();
+	    if(!expUrl.equals(actUrl)) {
+	    	fail("Page doesn't navigate");
+	    }
 	}
 }

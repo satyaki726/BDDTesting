1f7888c6414ea491bd1a0787d500340d1ff8a232
satyaki726 <satyakisaha726@gmail.com>
2023-01-10 19:32:53 +0530

    Added order to global and tagged hook

--- src/test/java/com/cts/bdd/steps/Common_Steps.java
+++ src/test/java/com/cts/bdd/steps/Common_Steps.java
@@ -9,19 +9,26 @@ import io.cucumber.java.Before;
 public class Common_Steps {
 
 	WebDriver driver;
-	
-	@Before
+
+	// In Before hook Higher Priority -> The hook that executed before rest of any
+	// other before hook
+	// In After hook Higher Priority -> The hook that executed after rest of any
+	// other after hook
+
+	@Before(order = 0)
 	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", "webdrivers/chromedriver.exe");
 		driver = new ChromeDriver();
+		System.out.println("Global Before Hook Got Exectued");
 	}
-	
-	@After
-	public void closeDown() throws Exception{
+
+	@After(order = 1)
+	public void closeDown() throws Exception {
 		driver.quit();
+		System.out.println("Global After Hook Got Exectued");
 		Thread.sleep(1000);
 	}
-	
+
 	public WebDriver getDriver() {
 		return driver;
 	}
